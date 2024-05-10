63bf2d24fcf0e522111cf0ec5a456c412181b591
sanalkumars <sanalskumar126@gmail.com>
2024-05-10 12:05:43 +0530

    meeting setup completed

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,50 @@
+import { cn } from '@/lib/utils';
+import { CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react'
+
+type CallLayoutType = 'grid' |'speaker-left' | 'speaker-right';
+
+const MeetingRoom = () => {
+
+
+  const[ layout , setLayout ] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
+
+  const CallLayout = () => {
+    switch (layout) {
+      case 'grid':
+        return <PaginatedGridLayout />;
+      case 'speaker-right':
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
+
+
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+            <CallLayout />
+        </div>
+        {/* the below div is for listing the participants */}
+        <div
+          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+            'show-block': showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+      
+      {/* video layout and call controls */}
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+
+      </div>
+
+    </section>
+  )
+}
+
+export default MeetingRoom
