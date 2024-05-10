fd1410d18de23c6366e0661998aa71a821089546
sanalkumars <sanalskumar126@gmail.com>
2024-05-10 13:09:09 +0530

    meeting room part completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,15 @@
 import StreamVideoProvider from '@/Providers/StreamClientProvider'
-import React, { ReactNode } from 'react'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react';
+
+
+export const metadata: Metadata = {
+  title: "MeetNow",
+  description: "Video Calling App",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
 
 const RootLayout = ({children}:{children: ReactNode} ) => {
   return (
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,14 +1,34 @@
 import { cn } from '@/lib/utils';
-import { CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react'
+import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LayoutList, Users } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+import EndCallButton from './EndCallButton';
+import Loader from './Loader';
 
-type CallLayoutType = 'grid' |'speaker-left' | 'speaker-right';
+
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
 
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get('personal');
 
-  const[ layout , setLayout ] = useState<CallLayoutType>("speaker-left");
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
+
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if( callingState !== CallingState.JOINED) return <Loader />
 
   const CallLayout = () => {
     switch (layout) {
@@ -26,7 +46,7 @@ const MeetingRoom = () => {
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
         <div className="flex size-full max-w-[1000px] items-center">
-            <CallLayout />
+          <CallLayout />
         </div>
         {/* the below div is for listing the participants */}
         <div
@@ -37,10 +57,40 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-      
+
       {/* video layout and call controls */}
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
+        <DropdownMenu>
+        <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                    
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
 
+        </DropdownMenu>
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+            <Users size={20} className="text-white" />
+          </div>
+        </button>
+        {!isPersonalRoom && <EndCallButton />}
       </div>
 
     </section>
