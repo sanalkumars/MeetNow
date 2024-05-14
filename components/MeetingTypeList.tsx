42410ef595e58dd9e56ec71b3eb4de9084495a8c
sanalkumars <sanalskumar126@gmail.com>
2024-05-14 11:52:21 +0530

    joining meeting via link is done

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,7 @@ import { useToast } from "@/components/ui/use-toast"
 import { Textarea } from './ui/textarea';
 
 import ReactDatePicker from "react-datepicker"
+import { Input } from './ui/input';
 
 const initialValues = {
   dateTime: new Date(),
@@ -155,6 +156,21 @@ const MeetingTypeList = () => {
           buttonText="Copy Meeting Link"
         />
       )}
+
+      <MeetingModel
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Meeting link"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </MeetingModel>
 
 
 
